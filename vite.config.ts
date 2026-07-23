70a13daa2c8dcd6756a6de43da656b8dc3ff13f7
kubaVIPy <kubavipy2008@gmail.com>
2026-07-23 19:43:13 +0200

    clean: remove duplicate code and migrate to TypeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './', // <--- This forces relative paths and works on all servers
   root: '.',
   publicDir: 'public',
   build: {
